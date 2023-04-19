96a215d74ba15dfd05723f30c96985e66f6aeb53
Harry Trimble <harryroberttrimble@gmail.com>
2023-04-19 12:00:27 +0100

    Add upload spreadsheet journey

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,13 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/uni/fill-in-proof-details', function(request, response) {
+
+    var inputtype = request.session.data['inputtype']
+    if (inputtype == "spreadsheet"){
+        response.redirect("/uni/upload-spreadsheet")
+    } else {
+        response.redirect("/uni/fill-in-proof-details")
+    }
+})
